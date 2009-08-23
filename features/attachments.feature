4640ff10aef1828bc16640953a8c4e1087dac021
Jason Garber <jg@jasongarber.com>
2009-08-23 06:34:49 -0400

    wip

---
+++ features/attachments.feature
@@ -0,0 +1,33 @@
+Feature: attachments
+  As a content editor
+  I should be able to add, reorder, and delete attachments
+  
+  Background:
+    Given I am logged in as admin
+  
+  Scenario: attach a file
+    Given I have a page with no attachments
+    When I edit the page
+    And I attach the Rails logo
+    And I save
+    Then the page should have a new attachment
+  
+  Scenario: don't change attachments
+    Given I have a page with 2 attachments
+    When I edit the page
+    And I save
+    Then the page should have 2 attachments
+  
+  Scenario: attach another file
+    Given I have a page with 2 attachments
+    When I edit the page
+    And I attach the Rails logo
+    And I save
+    Then the page should have 3 attachments
+  
+  Scenario: delete a file
+    Given I have a page with 2 attachments
+    When I edit the page
+    And I delete the first attachment
+    And I save
+    Then the page should have 1 attachment
--- features/step_definitions/attachment_steps.rb
+++ features/step_definitions/attachment_steps.rb
@@ -11,6 +11,11 @@ Given /^I have a page$/ do
   @page = pages(:home)
 end
 
+Given /^I have a page with (\d+) attachments$/ do |number|
+  Given "I have a page"
+  @page.attachments.length.should == number.to_i
+end
+
 Given /^I have a page with no attachments$/ do
   Given "I have a page"
   @page.attachments.destroy_all
@@ -22,13 +27,23 @@ When /^I edit the page$/ do
 end
 
 When /^I attach the Rails logo$/ do
-  attach_file "file_input", "#{PageAttachmentsExtension.root}/spec/fixtures/rails.png", "image/png" 
+  attach_file "file_input", "#{PageAttachmentsExtension.root}/spec/fixtures/rails.png"
+end
+
+When /^I delete the first attachment$/ do
+  set_hidden_field "page[attachments_attributes][1][_delete]", "1"
 end
 
 When /^I save$/ do
   click_button "Save"
+  assigns['page'].errors.should be_empty if assigns['page']
+  flash[:notice].should =~ /saved/
 end
 
 Then /^the page should have a new attachment$/ do
   @page.reload.attachments.should_not be_empty
+end
+
+Then /^the page should have (\d+) attachment(?:s)?$/ do |number|
+  @page.reload.attachments.length.should == number.to_i
 end
